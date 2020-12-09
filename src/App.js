49fa225378d07b507c9e40544aa6528510d14087
Tam Nguyen <tam.k.nguyen14@gmail.com>
2020-12-09 09:48:29 -0500

    created alternate forager page to experiment with styling

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Home from './components/pages/HomePage/Home.js';
 import About from './components/pages/AboutPage/About.js';
 import Resume from './components/pages/ResumePage/Resume.js';
 import Forager from './components/pages/ForagerPage/Forager.js';
+import Forager2 from './components/pages/ForagerPage/Forager2.js';
 import Vitro from './components/pages/VitroPage/Vitro.js';
 import Footer from './components/pages/Footer/Footer.js';
 
@@ -26,6 +27,7 @@ function App() {
         <Route path='/about' component={About}/>
         <Route path='/resume' component={Resume}/>
         <Route path='/forager' component={Forager}/>
+        <Route path='/forager2' component={Forager2}/>
         <Route path='/vitro' component={Vitro}/>
       </Switch>
       <Footer />
