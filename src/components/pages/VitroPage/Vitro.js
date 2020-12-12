c18bf09b1a396c8116dde46a28ec8bf0d7e85e4b
Tam Nguyen <tam.k.nguyen14@gmail.com>
2020-12-12 17:44:53 -0500

    fixed variable name issue in vitro project page

--- src/components/pages/VitroPage/Vitro.js
+++ src/components/pages/VitroPage/Vitro.js
@@ -11,7 +11,7 @@ function Vitro() {
             <div className='vitro'>
                 <div className='row'>
                     <div className='vitro__top-line'>Vitro View</div>
-                    <div className='vitro__intro'>{vitroViewObj.introduction}</div>
+                    <div className='vitro__intro'>{vitroObj.introduction}</div>
                 </div>
                 <div className='vitro__img-container'>
                     <img src={vitroViewImg} className='vitro__img'></img>
@@ -21,31 +21,31 @@ function Vitro() {
                         <div className='vitro__spec-wrapper'>
                             <div className='team-header'>Team Members</div>
                             <p className='team-names'>
-                                {vitroViewObj.teamMembers[0]}
+                                {vitroObj.teamMembers[0]}
                                 <br/>
-                                {vitroViewObj.teamMembers[1]}
+                                {vitroObj.teamMembers[1]}
                             </p>
                             <div className='role-header'>My Roles</div>
                             <p classname='roles'>
-                                {vitroViewObj.roles[0]}
+                                {vitroObj.roles[0]}
                                 <br/>
-                                {vitroViewObj.roles[1]}
+                                {vitroObj.roles[1]}
                             </p>
                             <div className='timeline-header'>Timeline</div>
                             <p classname='timeline'>
-                                {vitroViewObj.timeline}
+                                {vitroObj.timeline}
                             </p>
                             <div className='tools'>Tools</div>
                             <p className='tools'>
-                                {vitroViewObj.tools[0]}
+                                {vitroObj.tools[0]}
                                 <br/>
-                                {vitroViewObj.tools[1]}
+                                {vitroObj.tools[1]}
                             </p>
                         </div>
                     </div>
                     <div className='col'>
                         <div className='vitro__description-wrapper'>
-                            {vitroViewObj.introduction}
+                            {vitroObj.introduction}
                         </div>
                     </div>
                 </div>
