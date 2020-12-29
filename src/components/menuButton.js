7ef76ba3ccaa8538110c7f83f023bd1eae995aab
Tam Nguyen <tam.k.nguyen14@gmail.com>
2020-12-29 11:25:53 -0500

    test

---
+++ src/components/menuButton.js
@@ -0,0 +1,17 @@
+import './hamburgers.css';
+
+function activeMenuButton() {
+    <button class="hamburger hamburger--collapse is-active" type="button">
+        <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+        </span>
+    </button> 
+}
+
+function activeMenuButton() {
+    <button class="hamburger hamburger--collapse is-active" type="button">
+        <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+        </span>
+    </button> 
+}
